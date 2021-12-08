f44a896f94da9a60e3bd9163bf57dbbd7fca5836
Sanket Bhoyane <95101283+Sanket-Bhoyane@users.noreply.github.com>
2021-12-08 23:43:36 +0530

    Add files via upload

---
+++ Anudip Practicals/Array3.java
@@ -0,0 +1,10 @@
+public class Array3 {
+  public static void main(String[] args) {
+  // Write your code here
+int[] numbers=new int[]{20, 30, 25, 35, -16, 60, -100};
+int sum=0;
+for(int i=0;i<numbers.length;i++)
+sum=sum+numbers[i];
+double average=sum/numbers.length;
+System.out.println("Average value of the array element is:" +average);
+}}
---
+++ Anudip Practicals/Array4.java
@@ -0,0 +1,18 @@
+public class Array4 {
+  
+  // Write your code here
+public static boolean contains(int[] arr,int character){
+for(int n:arr){
+    if(character==n){
+      return true;
+    }}
+    return false;
+    }
+    public static void main(String[] args) {
+ int[] my_array={1789, 2035, 1899, 1456, 2013, 
+            1458, 2458, 1254, 1472, 2365, 
+            1456, 2265, 1457, 2456};
+            System.out.println(contains(my_array,2013));
+  System.out.println(contains(my_array,2015));
+}
+}
