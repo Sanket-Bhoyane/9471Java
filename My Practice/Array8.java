8ab815eddc2f1e5639c15eaa2f30b19135b504ee
Sanket Bhoyane <95101283+Sanket-Bhoyane@users.noreply.github.com>
2021-12-31 00:14:07 +0530

    Add files via upload

---
+++ My Practice/Array8.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Array8 {
+
+	public static void main(String[] args) {
+		
+
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the size of array:");
+	int n=sc.nextInt();
+	int[] a=new int[n];
+	System.out.println("enter the " +n+ " numbers");
+	for(int i=0;i<n;i++) {
+		a[i]=sc.nextInt();
+		
+	}
+	System.out.println("the " +n+ " numbers are:");
+	for(int i=0;i<n;i++) {
+		System.out.println(a[i]);
+	}
+	}
+
+}
