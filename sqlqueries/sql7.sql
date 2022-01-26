e0b3e6d62c8f00202e87f971825b8b0d3534d8d0
Sanket Bhoyane <95101283+Sanket-Bhoyane@users.noreply.github.com>
2022-01-26 18:50:06 +0530

    Add files via upload

---
+++ sqlqueries/sql7.sql
@@ -0,0 +1,10 @@
+insert into worker values(001,"Monika","Arora",100000,"14-02-20 09.00.00","HR");
+insert into worker values(002,"Niharika","Verma",80000,"14-06-11 09.00.00","ADMIN");
+insert into worker values(003,"Vishal","Singhal",300000,"14-02-20 09.00.00","HR");
+insert into worker values(004,"Amitabh","Singh",500000,"14-02-20 09.00.00","ADMIN");
+insert into worker values(005,"Vivek","Bhati",500000,"14-06-11 09.00.00","ADMIN");
+insert into worker values(006,"Vipul","Diwan",200000,"14-06-11 09.00.00","Account");
+insert into worker values(007,"Satish","Kumar",75000,"14-01-20 09.00.00","Account");
+insert into worker values(008,"Geetika","Chavan",90000,"14-04-11 09.00.00","ADMIN");
+select * from worker;
+Select LTRIM(DEPARTMENT) from worker;
