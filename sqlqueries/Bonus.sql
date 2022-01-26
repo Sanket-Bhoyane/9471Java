22b67246847a28b872536025c4bad92120791b1b
Sanket Bhoyane <95101283+Sanket-Bhoyane@users.noreply.github.com>
2022-01-26 14:48:55 +0530

    Add files via upload

---
+++ sqlqueries/Bonus.sql
@@ -0,0 +1,6 @@
+insert into Bonus values(001,5000,"16-02-20");
+insert into Bonus values(002,3000,"16-06-11");
+insert into Bonus values(003,4000,"16-02-20");
+insert into Bonus values(004,4500,"16-02-20");
+insert into Bonus values(005,3500,"16-06-11");
+select * from Bonus;
---
+++ sqlqueries/Title table.sql
@@ -0,0 +1,9 @@
+insert into Title values(001,"Manager","2016-02-20 00:00:00");
+insert into Title values(002,"Executive","2016-06-11 00:00:00");
+insert into Title values(008,"Executive","2016-06-11 00:00:00");
+insert into Title values(005,"Manager","2016-06-11 00:00:00");
+insert into Title values(004,"Asst.Manager","2016-06-11 00:00:00");
+insert into Title values(007,"Executive","2016-06-11 00:00:00");
+insert into Title values(006,"Lead","2016-06-11 00:00:00");
+insert into Title values(003,"Lead","2016-06-11 00:00:00");
+select * from Title;
