81c8efb7e66f16795f543c198ffafd6bae8d1d51
Sanket Bhoyane <95101283+Sanket-Bhoyane@users.noreply.github.com>
2022-02-17 23:42:07 +0530

    Add files via upload

---
+++ Anudip Projects/Spring Projects/Collection/src/com/Collection/MainApp.java
@@ -0,0 +1,20 @@
+package com.Collection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		  ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+	      JavaCollection jc=(JavaCollection)context.getBean("javaCollection");
+
+	      jc.getAddressList();
+	      jc.getAddressSet();
+	      jc.getAddressMap();
+	      jc.getAddressProp();
+	   }	}
+
+
