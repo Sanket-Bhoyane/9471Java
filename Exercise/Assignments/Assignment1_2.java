374d1d4634cd71682f4c9643f7eba616deb33b8d
Sanket Bhoyane <95101283+Sanket-Bhoyane@users.noreply.github.com>
2022-03-10 12:22:34 +0530

    Add files via upload

---
+++ Exercise/Assignments/Assignment1_2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Assignment1_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter The Strings:");
+		
+String a=sc.nextLine();
+String b=sc.nextLine();
+String c=sc.nextLine();
+boolean result=a.equalsIgnoreCase(b);
+boolean result1=a.equalsIgnoreCase(c);
+if(result==true) {
+	System.out.println("Yes,String a & b Are Same:");
+	
+}
+else 
+{	System.out.println("No,a & b Are Not Same:");
+
+	}
+if(result1==true) {
+	System.out.println("Yes,String a & c Are Same:");
+
+}
+else {
+	System.out.println("No,String a & c Are Not Same:");
+}
+	}
+
+}
---
+++ Exercise/Assignments/Assignment1_4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Assignment1_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter The String:");
+String n=sc.nextLine();
+
+int length=n.length();
+System.out.println("The Length Of Given String Is:");
+
+System.out.println(length);
+	}
+
+}
